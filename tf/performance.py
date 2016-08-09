476d58a4079faf812b18c0bc7c5ec110053d0f68
snow_leps <mattdns100689@gmail.com>
2016-08-09 11:21:44 +0100

    change to non notebook

--- tf/performance.py
+++ tf/performance.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import numpy.random as rng
 
 class performance():
     def __init__(self):
@@ -17,13 +18,21 @@ class performance():
         self.losses = []
         self.scores = []
 
-def display(x,yPred,yTruth):
-    imgs = np.vstack(tuple([row for row in x.squeeze()]))
-    preds = np.vstack(tuple([row for row in yPred.squeeze()]))
-    truth = np.vstack(tuple([row for row in yTruth.squeeze()]))
-    predsTruth = np.hstack((preds,truth))
+def display(x,yPred,yTruth,score,pickOne):
+    if pickOne ==0:
+        imgs = np.vstack(tuple([row for row in x.squeeze()]))
+        preds = np.vstack(tuple([row for row in yPred.squeeze()]))
+        truth = np.vstack(tuple([row for row in yTruth.squeeze()]))
+        predsTruth = np.hstack((preds,truth))
+    else:
+        obs = rng.randint(x.shape[0])
+        imgs = x[obs].squeeze()
+        preds = yPred[obs].squeeze() 
+        truth = yTruth[obs].squeeze() 
+        predsTruth = np.hstack((preds,truth))
     plt.subplot(121)
     plt.imshow(imgs,cmap=cm.gray)
     plt.subplot(122)
     plt.imshow(predsTruth,cmap=cm.gray)
+    plt.title("Score = %f" %score)
     plt.show()
